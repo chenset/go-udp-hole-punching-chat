a26a694099c32c3b2e12b14e3813a1413e4977b5
Ihor Omelchenko <counter3d@gmail.com>
2018-05-25 16:58:51 +0300

    Update client/main.go to OOP style

--- client/main.go
+++ client/main.go
@@ -15,68 +15,83 @@ type ChatRequest struct {
     Message string
 }
 
-func main() {
-    if len(os.Args) < 5 {
-        log.Fatal("Usage: ", os.Args[0], " port serverAddr username peername")
-    }
+type UDPChatClient struct {
+    addr *net.UDPAddr
+    serverAddr *net.UDPAddr
+    peerAddr *net.UDPAddr
+    username string
+    peername string
+    buffer []byte
+    connection *net.UDPConn
+}
 
-    port := fmt.Sprintf(":%s", os.Args[1])
-    serverAddr := os.Args[2]
-    username := os.Args[3]
-    peer := os.Args[4]
+func NewUDPChatClient(port string, server string, username string, peername string) *UDPChatClient {
+    log.Print("port: ", port, ", server: ", server, ", username: ", username, ", peer: ", peername)
 
-    log.Print("port: ", port, ", serverAddr: ", serverAddr, ", username: ", username, ", peer: ", peer)
+    client := new(UDPChatClient)
 
-    buf := make([]byte, 2048)
-
-    saddr, err := net.ResolveUDPAddr("udp4", serverAddr)
+    var err error
+    client.serverAddr, err = net.ResolveUDPAddr("udp4", server)
     if err != nil {
         log.Print("Resolve server address failed.")
         log.Fatal(err)
     }
-    log.Print("saddr: ", saddr)
 
-    addr, err := net.ResolveUDPAddr("udp4", port)
+    client.addr, err = net.ResolveUDPAddr("udp4", port)
     if err != nil {
-        log.Print("Resolve locl addres failed.")
+        log.Print("Resolve local addres failed.")
         log.Fatal(err)
     }
-    log.Print("addr: ", addr)
 
-    conn, err := net.ListenUDP("udp", addr)
+    client.username = username
+    client.peername = peername
+
+    client.buffer = make([]byte, 2048)
+
+    client.connection, err = net.ListenUDP("udp", client.addr)
     if err != nil {
         log.Print("Listen UDP failed.")
         log.Fatal(err)
     }
 
+    client.connectToServer()
+    client.getPeerAddress(peername)
+
+    return client
+}
+
+func (self *UDPChatClient) connectToServer() {
     initChatRequest := ChatRequest{
         "New",
-        username,
+        self.username,
         "",
     }
+
     jsonRequest, err := json.Marshal(initChatRequest)
     if err != nil {
         log.Print("Marshal Register information failed.")
         log.Fatal(err)
     }
-    _, err = conn.WriteToUDP(jsonRequest, saddr)
+    _, err = self.connection.WriteToUDP(jsonRequest, self.serverAddr)
     if err != nil {
         log.Fatal(err)
     }
 
     log.Print("Waiting for server response...")
-    _, _, err = conn.ReadFromUDP(buf)
+    _, _, err = self.connection.ReadFromUDP(self.buffer)
     if err != nil {
         log.Print("Register to server failed.")
         log.Fatal(err)
     }
+}
 
+func (self *UDPChatClient) getPeerAddress(peername string) *net.UDPAddr {
     connectChatRequest := ChatRequest{
         "Get",
-        username,
-        peer,
+        self.username,
+        peername,
     }
-    jsonRequest, err = json.Marshal(connectChatRequest)
+    jsonRequest, err := json.Marshal(connectChatRequest)
     if err != nil {
         log.Print("Marshal connection information failed.")
         log.Fatal(err)
@@ -84,13 +99,13 @@ func main() {
 
     var serverResponse ChatRequest
     for i := 0; i < 3; i++ {
-        conn.WriteToUDP(jsonRequest, saddr)
-        n, _, err := conn.ReadFromUDP(buf)
+        self.connection.WriteToUDP(jsonRequest, self.serverAddr)
+        n, _, err := self.connection.ReadFromUDP(self.buffer)
         if err != nil {
             log.Print("Get peer address from server failed.")
             log.Fatal(err)
         }
-        err = json.Unmarshal(buf[:n], &serverResponse)
+        err = json.Unmarshal(self.buffer[:n], &serverResponse)
         if err != nil {
             log.Print("Unmarshal server response failed.")
             log.Fatal(err)
@@ -104,47 +119,74 @@ func main() {
     if serverResponse.Message == "" {
         log.Fatal("Cannot get peer's address")
     }
-    log.Print("Peer address: ", serverResponse.Message)
-    peerAddr, err := net.ResolveUDPAddr("udp4", serverResponse.Message)
+    log.Print("Peer ", peername, " address: ", serverResponse.Message)
+    self.peerAddr, err = net.ResolveUDPAddr("udp4", serverResponse.Message)
     if err != nil {
         log.Print("Resolve peer addres failed.")
         log.Fatal(err)
     }
 
-    go listen(conn)
+    return self.peerAddr
+}
+
+func (self *UDPChatClient) start() {
+    go self.listen(self.connection)
+
+    self.handleInput()
+}
+
+func (self *UDPChatClient) handleInput() {
     for {
         fmt.Print("Input message: ")
         message := make([]byte, 2048)
         fmt.Scanln(&message)
         messageRequest := ChatRequest{
             "Chat",
-            username,
+            self.username,
             string(message),
         }
-        jsonRequest, err = json.Marshal(messageRequest)
+        jsonRequest, err := json.Marshal(messageRequest)
         if err != nil {
             log.Print("Error: ", err)
             continue
         }
-        conn.WriteToUDP(jsonRequest, peerAddr)
+        self.connection.WriteToUDP(jsonRequest, self.peerAddr)
     }
 }
 
-func listen(conn *net.UDPConn) {
+func (self *UDPChatClient) listen(conn *net.UDPConn) {
     for {
-        buf := make([]byte, 2048)
-        n, _, err := conn.ReadFromUDP(buf)
+        self.buffer = make([]byte, 2048)
+        n, _, err := conn.ReadFromUDP(self.buffer)
         if err != nil {
             log.Print(err)
             continue
         }
 
         var message ChatRequest
-        err = json.Unmarshal(buf[:n], &message)
+        err = json.Unmarshal(self.buffer[:n], &message)
         if err != nil {
             log.Print(err)
             continue
         }
-        fmt.Println(message.Username, ":", message.Message)
+        fmt.Println("\n", message.Username, ":", message.Message)
+        fmt.Print("Input message: ")
     }
 }
+
+func main() {
+    if len(os.Args) < 5 {
+        log.Fatal("Usage: ", os.Args[0], " port serverAddr username peername")
+    }
+
+    port := os.Args[1]
+    serverAddr := os.Args[2]
+    username := os.Args[3]
+    peer := os.Args[4]
+
+    log.Print("port: ", port, ", serverAddr: ", serverAddr, ", username: ", username, ", peer: ", peer)
+
+    client := NewUDPChatClient(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+    client.start()
+}
+
